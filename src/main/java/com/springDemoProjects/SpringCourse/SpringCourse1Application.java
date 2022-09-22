398d281e993bb3f52cefe1d422998b0966a3dca3
anandh2001 <anandh.andrews@zucisystems.com>
2022-09-22 23:20:56 +0530

    new spring project

---
+++ src/main/java/com/springDemoProjects/SpringCourse/SpringCourse1Application.java
@@ -0,0 +1,13 @@
+package com.springDemoProjects.SpringCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourse1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springDemoProjects/SpringCourse/SpringCourse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDemoProjects.SpringCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
