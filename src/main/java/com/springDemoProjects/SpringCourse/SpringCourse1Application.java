283f51b017853e29f90310a820ebb570e33a2e14
anandh2001 <anandh.andrews@zucisystems.com>
2022-09-23 20:46:03 +0530

    new spring project

--- src/main/java/com/springDemoProjects/SpringCourse/SpringCourse1Application.java
+++ src/main/java/com/springDemoProjects/SpringCourse/SpringCourse1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCourse1Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourse1Application.class, args);
 	}
